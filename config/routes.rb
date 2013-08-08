20807684c54f29d294da8c3605ff5dc867447af3
Requiem- <e.ungureanu92@gmail.com>
2013-08-08 13:53:59 +0300

    Added Projects controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Javazone::Application.routes.draw do
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
+  resources :projects
 end
