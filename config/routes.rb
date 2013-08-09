21210520c2d3ecaa809f056f95b947c5382ed4e6
Requiem- <e.ungureanu92@gmail.com>
2013-08-09 13:44:57 +0300

    The project page show the users' time entry info

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Javazone::Application.routes.draw do
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
+ 
   resources :users
   resources :projects
 end
