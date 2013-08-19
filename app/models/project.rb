1c134e022a428ea0c03f83ed29b2577cbe0281e8
Requiem- <e.ungureanu92@gmail.com>
2013-08-19 15:43:47 +0300

    Admin can't change his role

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ActiveRecord::Base
-  validates :name, uniqueness: true, length: { in: 2..30 }
+  validates :name, uniqueness: true, length: { in: 2..40 }
   has_many :time_entries
 end
