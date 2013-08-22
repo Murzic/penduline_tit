17251e3835fb1b7499613d9251d5df124efdc6ac
Requiem- <e.ungureanu92@gmail.com>
2013-08-22 15:55:21 +0300

    Changed the mailer views to haml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Penduline_tit::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { :host => 'http://powerful-ravine-3993.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'http://penduline-tit.herokuapp.com/' }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
